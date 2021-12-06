777f2a935d7dc4254d63e5f7381e4f752dfab9ae
schminkman <ethanliles@yahoo.com>
2021-12-06 16:27:58 -0600

    removed unused files / components / screens

--- App.js
+++ App.js
@@ -1,29 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 
-import Screen from "./app/components/Screen";
-import InfoScreen from "./app/screens/InfoScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import RecommendScreen from "./app/screens/RecommendScreen";
-import MapScreen from "./app/screens/MapScreen";
-import LogScreen from "./app/screens/LogScreen";
-import Card from "./app/components/Card";
-import colors from "./app/config/colors";
-import AppNavigator from "./app/navigation/AppNavigator";
 import AuthNavigator from "./app/navigation/AuthNavigator";
-import AppTextInput from "./app/components/AppTextInput";
-import AddScreen from "./app/screens/AddSightingScreen";
-import AppPicker from "./app/components/AppPicker";
-import LoginScreen from "./app/screens/LoginScreen";
-import SignupScreen from "./app/screens/SignupScreen";
-import A_InfoScreen from "./app/admin/A_InfoScreen";
-import AdminNavigator from "./app/navigation/AdminNavigator";
 
+// the App component, which handles the entire app
+// the only thing rendered here is the auth navigator which then navigates to either the AppNavigator  (for typical users)
+// or the AdminNavigator (for admins)
 export default function App() {
   return (
     <NavigationContainer>
@@ -31,12 +13,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
